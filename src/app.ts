f41ca056934ecfac3f47c4670d7c8e1c518cf290
Andre Luiz Rabello <andre.luiz_r@hotmail.com>
2019-03-04 19:52:11 +0100

    refactor: global styles

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import { html } from 'lit-html';
-import './app.scss';
 
 export const App = html`
   <h1>Hello world!</h1>
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { render } from 'lit-html';
 import { App } from './app';
+import './app.scss';
 
 render(App, document.body);
